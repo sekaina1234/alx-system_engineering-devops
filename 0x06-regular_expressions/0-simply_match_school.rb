124cd68f1efeb4c8c2cf89409042a04df1b214f7
sekaina123 <soukainasaoud54@gmail.com>
2023-08-01 09:59:56 -0700

    0. Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+def find_and_print_school_occurrences(input_string)
+  occurrences = input_string.scan(/School/)
+  puts occurrences.join
+end
+input_argument = ARGV[0]
+find_and_print_school_occurrences(input_argument)
