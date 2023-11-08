a73d8b0649a6e9cdba6818c6ea2274f977b9bae0
sekaina1234 <soukainasaoud54@gmail.com>
2023-11-08 01:22:08 -0800

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,26 +8,15 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Get the number of subscribers for a given subreddit
-    """ 
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyBot/1.0 (by YourUsername)"}
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        
-        if response.status_code == 200:
-            return data["data"]["subscribers"]
-        else:
-            return 0
-    except:
+    if response.status_code != 200:
         return 0
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print(subscribers)
+    data = response.json().get("data")
+    num_subs = data.get("subscribers")
+
+    return num_subs
